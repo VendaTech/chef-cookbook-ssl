f7f1e2014ab1a2a7a07ebebc54a0b6bd2c861962
Chris Andrews <chris@nodnol.org>
2012-09-12 18:39:19 +0100

    Add "localca" recipe.
    This deploys all the CAs it can find among your deployed certificates to
    the OpenSSL certificate store.

---
+++ recipes/localca.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook Name:: ssl
+# Recipe:: localca
+#
+# Copyright 2012 Venda Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+node['cacerts'] ||= {}
+
+certs_dir = '/etc/pki/tls/certs' # XXX platform dependent
+
+search('certificates') do |item|
+
+  if !item['cacert'].nil?
+    cert = OpenSSL::X509::Certificate.new(item['cacert'])
+    Chef::Log.info(cert.subject)
+
+    hash = sprintf("%x", cert.subject.hash)
+    hash_path = File.join(certs_dir, "#{hash}.0")
+
+    if !node['cacerts'].has_key?(hash_path) || node['cacerts'][hash_path] != item['cacert']
+
+      file hash_path do
+        content item['cacert']
+        action :create
+        owner "root"
+        group "root"
+        mode 0644
+      end
+
+      node['cacerts'][hash_path] = item['cacert']
+    end
+  end
+end
