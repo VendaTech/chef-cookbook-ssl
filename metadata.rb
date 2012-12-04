da2e5042186f29950e15a9e3b37046f464fcbd26
Chris Andrews <chris@nodnol.org>
2012-12-04 11:35:40 +0000

    Bump version number.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "candrews@venda.com"
 license          "Apache"
 description      "Deploy a Chef-managed Certificate Authority"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 depends 'gpg'
