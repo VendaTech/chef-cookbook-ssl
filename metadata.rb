465faf58b38597d7ccc0457a5af48eefa36bbfe3
Chris Andrews <chris@nodnol.org>
2013-03-12 10:44:42 +0000

    Add name to metadata, and update dependency to "vt-gpg".
    Preparing for publish to community site.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name             "ssl"
 maintainer       "Venda Ltd"
 maintainer_email "candrews@venda.com"
 license          "Apache"
 description      "Deploy a Chef-managed Certificate Authority"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.5"
+version          "1.0.6"
 
-depends 'gpg'
+depends 'vt-gpg'
