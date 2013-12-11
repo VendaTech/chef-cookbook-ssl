63cc6d8257c70bfd3618b2d38852bc5c7e254b99
Chris Andrews <chris@nodnol.org>
2013-12-11 09:43:56 +0000

    Version 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "candrews@venda.com"
 license          "Apache"
 description      "Deploy a Chef-managed Certificate Authority"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
+version          "1.1.0"
 
 depends 'vt-gpg'
